5ee07c0d3377f9ab8a9cd50020443ab55cdafbda
kgolikov <kgolikov@37bytes.com>
2022-01-14 15:35:49 +0500

    Content-Disposition usage example

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.get('/download/file.html', (req, res) => {
+    res.set('Content-Disposition', 'attachment; filename=file.html');
+    res.sendFile(path.resolve('./file.html'));
+});
+
+app.get('/view/file.html', (req, res) => {
+    res.set('Content-Disposition', 'inline'); //значение по умолчанию
+    res.sendFile(path.resolve('./file.html'));
+});
+
+app.listen(3234, () => {
+    console.log('http://localhost:3234')
+});
